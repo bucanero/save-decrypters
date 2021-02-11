191bcfe6ebaade2de3a16789ae980989d854677f
Damian Parrino <dparrino@gmail.com>
2021-02-11 16:15:10 -0300

    Diablo 3 decrypter

---
+++ diablo3-decrypter/README.md
@@ -0,0 +1,11 @@
+# diablo3-ps3save-decrypter
+
+A tool to decrypt Diablo III PS3 save-games
+
+```
+USAGE: ./diablo3-ps3save-decrypter [option] filename
+
+OPTIONS        Explanation:
+ -d            Decrypt File
+ -e            Encrypt File
+```
---
+++ diablo3-decrypter/main.c
@@ -0,0 +1,146 @@
+/*
+*
+*	Diablo III PS3 Save Decrypter - (c) 2021 by Bucanero - www.bucanero.com.ar
+*
+* This tool is based (reversed) on the original diablo_3_save_data_decrypter by Red-EyeX32
+*
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define u8 uint8_t
+#define u32 uint32_t
+
+#define DIABLO3_KEY1		0x305f92d8
+#define DIABLO3_KEY2		0x2ec9a01b
+
+int read_buffer(const char *file_path, u8 **buf, size_t *size)
+{
+	FILE *fp;
+	u8 *file_buf;
+	size_t file_size;
+	
+	if ((fp = fopen(file_path, "rb")) == NULL)
+        return -1;
+	fseek(fp, 0, SEEK_END);
+	file_size = ftell(fp);
+	fseek(fp, 0, SEEK_SET);
+	file_buf = (u8 *)malloc(file_size);
+	fread(file_buf, 1, file_size, fp);
+	fclose(fp);
+	
+	if (buf)
+        *buf = file_buf;
+	else
+        free(file_buf);
+	if (size)
+        *size = file_size;
+	
+	return 0;
+}
+
+int write_buffer(const char *file_path, u8 *buf, size_t size)
+{
+	FILE *fp;
+	
+	if ((fp = fopen(file_path, "wb")) == NULL)
+        return -1;
+	fwrite(buf, 1, size, fp);
+	fclose(fp);
+	
+	return 0;
+}
+
+void decrypt_data(u8* data, u32 size)
+{
+	u32 xor_key1 = DIABLO3_KEY1;
+	u32 xor_key2 = DIABLO3_KEY2;
+	u32 tmp;
+
+	printf("[*] Total Decrypted Size Is 0x%X (%d bytes)\n", size, size);
+
+	for (int i = 0; i < size; i++)
+	{
+		data[i] ^= (xor_key1 & 0xFF);
+		tmp = data[i] ^ xor_key1;
+		xor_key1 = xor_key1 >> 8 | xor_key2 << 0x18;
+		xor_key2 = xor_key2 >> 8 | tmp << 0x18;
+	}
+
+	printf("[*] Decrypted File Successfully!\n\n");
+	return;
+}
+
+void encrypt_data(u8* data, u32 size)
+{
+	u32 xor_key1 = DIABLO3_KEY1;
+	u32 xor_key2 = DIABLO3_KEY2;
+	u32 tmp;
+
+	printf("[*] Total Encrypted Size Is 0x%X (%d bytes)\n", size, size);
+
+	for (int i = 0; i < size; i++)
+	{
+		tmp = data[i] ^ xor_key1;
+		data[i] ^= (xor_key1 & 0xFF);
+		xor_key1 = xor_key1 >> 8 | xor_key2 << 0x18;
+		xor_key2 = xor_key2 >> 8 | tmp << 0x18;
+	}
+
+	printf("[*] Encrypted File Successfully!\n\n");
+	return;
+}
+
+void print_usage(const char* argv0)
+{
+	printf("USAGE: %s [option] filename\n\n", argv0);
+	printf("OPTIONS        Explanation:\n");
+	printf(" -d            Decrypt File\n");
+	printf(" -e            Encrypt File\n\n");
+	return;
+}
+
+int main(int argc, char **argv)
+{
+	size_t len;
+	u8* data;
+	char *opt, *bak;
+
+	printf("\ndiablo3-ps3save-decrypter 0.1.0 - (c) 2021 by Bucanero\n\n");
+
+	if (--argc < 2)
+	{
+		print_usage(argv[0]);
+		return -1;
+	}
+	
+	opt = argv[1];
+	if (*opt++ != '-' || (*opt != 'd' && *opt != 'e'))
+	{
+		print_usage(argv[0]);
+		return -1;
+	}
+
+	if (read_buffer(argv[2], &data, &len) != 0)
+	{
+		printf("[*] Could Not Access The File (%s)\n", argv[2]);
+		return -1;
+	}
+	// Save a file backup
+	asprintf(&bak, "%s.bak", argv[2]);
+	write_buffer(bak, data, len);
+
+	if (*opt == 'd')
+		decrypt_data(data, len);
+	else
+		encrypt_data(data, len);
+
+	write_buffer(argv[2], data, len);
+
+	free(bak);
+	free(data);
+	
+	return 0;
+}
