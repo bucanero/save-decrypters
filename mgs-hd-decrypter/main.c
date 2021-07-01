a1468e97fc23c131b0bde3bdb69c62a498fcd6cc
Damian Parrino <dparrino@gmail.com>
2021-07-01 10:05:17 -0300

    Code clean-up

--- mgs-hd-decrypter/main.c
+++ mgs-hd-decrypter/main.c
@@ -60,294 +60,137 @@ u32 crc32(u8* input, u32 len)
 	return ~crc;
 }
 
-u8* Decrypt(const char* key, u8* data, int offset, int size)
+void Decrypt(const char* key, u8* data, int size)
 {
-    u8 num = 0;
-    for (int i = offset; i < offset + size; i++)
-    {
-        u8 num1 = (u8)(data[i] - key[(i - offset) % strlen(key)] - (char)num);
-        num = data[i];
-        data[i] = num1;
-    }
-    return data;
+    int keylen = (key[0] ? strlen(key) : 1);
+
+    for (int i = size - 1; i > 0; i--)
+        data[i] -= (key[i % keylen] + data[i-1]);
+    
+    data[0] -= key[0];
+    return;
 }
 
-u8* DecryptLayer2(u8* data, int offset, int size, int salt)
+void Encrypt(const char* key, u8* data, int size)
 {
-    u8 num = 0;
-    for (int i = offset; i < offset + size; i++)
-    {
-//        salt = Routines.GetNextSalt(salt);
-        u8 num1 = (u8)(data[i] - salt - num);
-        num = data[i];
-        data[i] = num1;
-    }
-    return data;
+    int keylen = (key[0] ? strlen(key) : 1);
+    data[0] += key[0];
+
+    for (int i = 1; i < size; i++)
+        data[i] += (key[i % keylen] + data[i-1]);
+
+    return;
 }
 
-u8* Encrypt(const char* key, u8* data, int offset, int size)
+void EncodeBase64(u8* data, const char* chars)
 {
-    u8 num = 0;
-    for (int i = offset; i < offset + size; i++)
+    int i, j, k;
+    u8 tmpArray[28];
+
+    data[20] = 0;
+    for (j = 0; j < 20; j++)
+        data[20] ^= data[j];
+
+    for (i = 0, j = 0, k = 0; j < 28; j++)
     {
-        num = (u8)(data[i] + key[(i - offset) % strlen(key)] + (char)num);
-        data[i] = num;
+        if (k == 0 || k == 1)
+            tmpArray[j] = (u8)(data[i] >> (2-k));
+
+        else if (k == 2)
+            tmpArray[j] = (u8)(data[i] & 63);
+
+        else if (k <= 7)
+                tmpArray[j] = (u8)(data[i + 1] >> (10-k)) | ((data[i] & ((1 << (8-k)) - 1)) << (k-2));
+
+        k += 6;
+        if (k >= 8)
+        {
+            k -= 8;
+            i++;
+        }
     }
-    return data;
+
+    data[0] = 68;
+//    for (i = 0, j = 0; i < 28; i++, j += (i % 4) ? 0 : 28)
+//        data[i + 1] = (u8)chars[(tmpArray[i] + j +  7 * (i % 4)) & 63];
+
+    for (i = 0, j = 0; i < 28; j += 28, i += 4)
+    {
+        data[i + 1] = (u8)chars[(tmpArray[i] + j) & 63];
+        data[i + 2] = (u8)chars[(tmpArray[i + 1] + j +  7) & 63];
+        data[i + 3] = (u8)chars[(tmpArray[i + 2] + j + 14) & 63];
+        data[i + 4] = (u8)chars[(tmpArray[i + 3] + j + 21) & 63];
+    }
+
+    return;
 }
 
-u8* EncryptLayer2(u8* data, int offset, int size, int salt)
+void DecodeBase64(u8* data, const char* str)
 {
-    u8 num = 0;
-    for (int i = offset; i < offset + size; i++)
-    {
-//        salt = Routines.GetNextSalt(salt);
-        num = (u8)(data[i] + salt + num);
-        data[i] = num;
-    }
-    return data;
-}
+    int i, j, k, m;
+    u8 b64_table[0x100];
+    u8 tmpArray[0x20];
 
-u8* EncodeBase64(u8* data, const char* chars)
-{
-    int num;
-    int num1;
-    int j;
-    u8* numArray = data;
-    u8 num2 = 0;
-    for (int i = 0; i < 20; i++)
-    {
-        num2 = (u8)(num2 ^ data[i]);
-    }
-    data[20] = num2;
-    u8 numArray1[28];
-    int num3 = 0;
-    int num4 = 0;
-    int num5 = 0;
-    int num6 = 0;
-    for (j = 0; j < 28; j++)
-    {
-        if (num3 <= 7)
-        {
-            switch (num3)
-            {
-                case 0:
-                {
-                    numArray1[j] = (u8)(numArray[num4] >> 2);
-                    break;
-                }
-                case 1:
-                {
-                    numArray1[j] = (u8)(numArray[num4] >> 1);
-                    break;
-                }
-                case 2:
-                {
-                    numArray1[j] = (u8)(numArray[num4] & 63);
-                    break;
-                }
-                case 3:
-                {
-                    num1 = numArray[num4 + 1];
-                    num = numArray[num4];
-                    num6 = num1 >> 7;
-                    num5 = (num & 31) << 1;
-                    numArray1[j] = (u8)(num5 | num6);
-                    break;
-                }
-                case 4:
-                {
-                    num1 = numArray[num4 + 1];
-                    num = numArray[num4];
-                    num6 = num1 >> 6;
-                    num5 = (num & 15) << 2;
-                    numArray1[j] = (u8)(num5 | num6);
-                    break;
-                }
-                case 5:
-                {
-                    num1 = numArray[num4 + 1];
-                    num = numArray[num4];
-                    num6 = num1 >> 5;
-                    num5 = (num & 7) << 3;
-                    numArray1[j] = (u8)(num5 | num6);
-                    break;
-                }
-                case 6:
-                {
-                    num1 = numArray[num4 + 1];
-                    num = numArray[num4];
-                    num6 = num1 >> 4;
-                    num5 = (num & 3) << 4;
-                    numArray1[j] = (u8)(num5 | num6);
-                    break;
-                }
-                case 7:
-                {
-                    num1 = numArray[num4 + 1];
-                    num = numArray[num4];
-                    num6 = num1 >> 3;
-                    num5 = (num & 1) << 5;
-                    numArray1[j] = (u8)(num5 | num6);
-                    break;
-                }
-                case 8:
-                {
-                    numArray1[j] = (u8)(num5 | num6);
-                    break;
-                }
-            }
-        }
-        num3 += 6;
-        if (num3 >= 8)
-        {
-            num3 -= 8;
-            num4++;
-        }
-    }
-    j = 7;
-    num4 = 0;
-    j = 0;
-    u8* numArray2 = malloc(32);
-    numArray2[0] = 68;
-    for (int k = 0; k < 7; k++)
-    {
-        int num7 = j + 21;
-        num = numArray1[num4];
-        num6 = numArray1[num4 + 1];
-        int num8 = num + j;
-        num = numArray1[num4 + 2];
-        num6 += j;
-        num5 = numArray1[num4 + 3];
-        num8 &= 63;
-        num3 = num + num7;
-        num = num6 + 7;
-        num6 = num3 - 7;
-        num3 = num & 63;
-        num = chars[num8];
-        num5 += num7;
-        num7 = num6 & 63;
-        num6 = num5 & 63;
-        j += 28;
-        numArray2[num4 + 1] = (u8)num;
-        num5 = chars[num3];
-        numArray2[num4 + 2] = (u8)num5;
-        num3 = chars[num7];
-        numArray2[num4 + 3] = (u8)num3;
-        num7 = chars[num6];
-        numArray2[num4 + 4] = (u8)num7;
-        num4 += 4;
-    }
-    return numArray2;
-}
+    memset(tmpArray, 0, sizeof(tmpArray));
+    memset(b64_table, 0xFF, sizeof(b64_table));
 
-u8* DecodeBase64(u8* data, const char* str)
-{
-    int num;
-    int num1;
-    int l;
-    int num2;
-    int num3;
-    int num4;
-    u8* numArray = data;
-/*
-    for (int i = 0; i < 28; i++)
+    for (k = 0; k < 64; k++)
+        b64_table[str[k]] = (u8)k;
+
+    for (j = 0, m = 0; j < 196; j += 7, m++)
     {
-        if (numArray[1 + i] == 95)
+        k = b64_table[data[1 + m]];
+
+        if (k == 0xff)
+            return;
+
+        data[m] = (u8)(k - j & 63);
+    }
+
+    for (j = 0, k = 0, m = 0; m < 21; m++)
+    {
+        if (j <= 5)
+            tmpArray[m] = (u8)(data[k] & 63 >> (j & 31)) << (2 + j & 31);
+
+        i = ~j + 7;
+        j = 0;
+        k++;
+
+        if (i < 2)
         {
-            numArray[1 + i] = 43;
+            tmpArray[m] |= (u8)(data[k] << ((~i + 3) & 31));
+            i += 6;
+            k++;
+        }
+        i -= 2;
+
+        if (i == 0)
+        {
+            tmpArray[m] |= (u8)(data[k]);
+            k++;
+            j = 0;
+        }
+        else if (i <= 5)
+        {
+            j = 6 - i;
+            tmpArray[m] |= (u8)(data[k] >> (i & 31));
         }
     }
-*/
-    u8 numArray1[0x100];
-    for (int j = 0; j < 0x100; j++)
-    {
-        numArray1[j] = 0xff;
-    }
-//    const char str[] = "ghijklmn01234567opqrstuvEFGHIJKL89abcdefUVWXYZ+-wxyzABCDMNOPQRST";
-    for (int k = 0; k < 64; k++)
-    {
-        numArray1[str[k]] = (u8)k;
-    }
-    int m = 0;
-    u8 numArray2[32];
-    for (l = 0; l < 196; l += 7)
-    {
-        num = numArray1[numArray[1 + m]];
-        numArray2[m] = (u8)num;
-        if (num == 0xff)
-        {
-            return NULL;
-        }
-        num4 = num - l & 63;
-        int num5 = m;
-        m = num5 + 1;
-        numArray2[num5] = (u8)num4;
-    }
-    u8 numArray3[21];
-    l = 0;
-    num = 0;
-    for (m = 0; m < 21; m++)
-    {
-        if (l <= 5)
-        {
-            num1 = numArray2[num];
-            num3 = (num1 & 63 >> (l & 31)) << (2 + l & 31);
-            numArray3[m] = (u8)num3;
-        }
-        num1 = ~l + 7;
-        l = 0;
-        num++;
-        if (num1 < 2)
-        {
-            num4 = numArray2[num];
-            int num6 = ~num1 + 3;
-            num1 += 6;
-            num3 = num4 << (num6 & 31);
-            num6 = numArray3[m];
-            num++;
-            numArray3[m] = (u8)(num3 | num6);
-        }
-        num1 -= 2;
-        if (num1 == 0)
-        {
-            int num7 = num;
-            num = num7 + 1;
-            num2 = numArray2[num7];
-            l = 0;
-            numArray3[m] = (u8)(num2 | numArray3[m]);
-        }
-        else if (num1 <= 5)
-        {
-            num2 = numArray2[num];
-            l = 6 - num1;
-            num3 = (u8)(num2 >> (num1 & 31));
-            numArray3[m] = (u8)(num3 | numArray3[m]);
-        }
-    }
-    u8* numArray4 = malloc(sizeof(numArray3));
-    memcpy(numArray4, numArray3, sizeof(numArray3));
-/*
-    num = 0;
-    for (int n = 0; n < 20; n++)
-    {
-        num1 = numArray3[n];
-        num2 = num1;
-        num = num1 ^ num;
-        numArray4[n] = (u8)num1;
-    }
-*/
-    return numArray4;
+    memcpy(data, tmpArray, sizeof(tmpArray));
+
+    return;
 }
 
 void mgs2_decrypt_data(u8* data, u32 size)
 {
 	printf("[*] MGS2 Total Decrypted Size Is 0x%X (%d bytes)\n", size, size);
 
-    Decrypt(MGS2_KEY, data, 4, 0x71a6);
-    DecryptLayer2(data, 4, 0x1598, 0);
-    DecryptLayer2(data, 0x15aa, 0x1c00, 0);
-    DecryptLayer2(data, 0x31aa, 0x4000, 0);
+    Decrypt(MGS2_KEY, data + 4, 0x71a6);
+
+    // Decrypt Layer 2
+    Decrypt("", data + 4, 0x1598);
+    Decrypt("", data + 0x15aa, 0x1c00);
+    Decrypt("", data + 0x31aa, 0x4000);
 
 	printf("[*] Decrypted File Successfully!\n\n");
 	return;
@@ -360,12 +203,13 @@ u32 mgs2_encrypt_data(u8* data, u32 size)
     u32 crc = ES32(CalculateCRC32(data, 4, 0x1598) ^ CalculateCRC32(data, 0x15aa, 0x1c00));
     memcpy(data + 0x15a6L, &crc, sizeof(u32));
 
-    EncryptLayer2(data, 4, 0x1598, 0);
-    EncryptLayer2(data, 0x15aa, 0x1c00, 0);
-    EncryptLayer2(data, 0x31aa, 0x4000, 0);
+    // Encrypt Layer 2
+    Encrypt("", data + 4, 0x1598);
+    Encrypt("", data + 0x15aa, 0x1c00);
+    Encrypt("", data + 0x31aa, 0x4000);
 
     crc = crc32(data + 4, 0x71a6);
-    Encrypt(MGS2_KEY, data, 4, 0x71a6);
+    Encrypt(MGS2_KEY, data + 4, 0x71a6);
 
     printf("[*] New Checksum: %08X\n", crc);
 	printf("[*] Encrypted File Successfully!\n\n");
@@ -376,7 +220,7 @@ void mgs3_decrypt_data(u8* data, u32 size)
 {
 	printf("[*] MGS3 Total Decrypted Size Is 0x%X (%d bytes)\n", size, size);
 
-    Decrypt(MGS3_KEY, data, 0, 0x497c);
+    Decrypt(MGS3_KEY, data, 0x497c);
 
 	printf("[*] Decrypted File Successfully!\n\n");
 	return;
@@ -387,7 +231,7 @@ u32 mgs3_encrypt_data(u8* data, u32 size)
 	printf("[*] MGS3 Total Encrypted Size Is 0x%X (%d bytes)\n", size, size);
 
     u32 crc = crc32(data, 0x497c);
-    Encrypt(MGS3_KEY, data, 0, 0x497c);
+    Encrypt(MGS3_KEY, data, 0x497c);
 
     printf("[*] New Checksum: %08X\n", crc);
 	printf("[*] Encrypted File Successfully!\n\n");
@@ -407,7 +251,6 @@ int main(int argc, char **argv)
 {
 	size_t len;
 	u8* data;
-	u8* master;
 	char *opt, *bak, type;
 
 	printf("\nMetal Gear Solid HD save decrypter 0.1.0 - (c) 2021 by Bucanero\n\n");
@@ -454,9 +297,8 @@ int main(int argc, char **argv)
 
 		write_buffer("MASTER.BIN.bak", data, len);
 
-		master = DecodeBase64(data, MGS2_ALPHABET);
-		write_buffer("MASTER.BIN", master, 21);
-		free(master);
+		DecodeBase64(data, MGS2_ALPHABET);
+		write_buffer("MASTER.BIN", data, len);
 
 		printf("[*] %s File Decoded\n", "MASTER.BIN");
 	}
@@ -476,9 +318,8 @@ int main(int argc, char **argv)
 		write_buffer("MASTER.BIN.bak", data, len);
 	
 	    memcpy(data + 16, &crc, sizeof(u32));
-	    master = EncodeBase64(data, MGS2_ALPHABET);
-		write_buffer("MASTER.BIN", master, 32);
-		free(master);
+	    EncodeBase64(data, MGS2_ALPHABET);
+		write_buffer("MASTER.BIN", data, len);
 
 		printf("[*] %s File Encoded\n", "MASTER.BIN");
 	}
@@ -497,9 +338,8 @@ int main(int argc, char **argv)
 
 		write_buffer("MASTER.BIN.bak", data, len);
 
-		master = DecodeBase64(data, MGS3_ALPHABET);
-		write_buffer("MASTER.BIN", master, 21);
-		free(master);
+		DecodeBase64(data, MGS3_ALPHABET);
+		write_buffer("MASTER.BIN", data, len);
 
 		printf("[*] %s File Decoded\n", "MASTER.BIN");
 	}
@@ -520,10 +360,9 @@ int main(int argc, char **argv)
 
 	    memcpy(data + 8, &crc, sizeof(u32));
 	    memcpy(data + 16, &crc, sizeof(u32));
-		master = EncodeBase64(data, MGS3_ALPHABET);
-		master[0] = 0x5F;
-		write_buffer("MASTER.BIN", master, 32);
-		free(master);
+		EncodeBase64(data, MGS3_ALPHABET);
+		data[0] = 0x5F;
+		write_buffer("MASTER.BIN", data, len);
 
 		printf("[*] %s File Encoded\n", "MASTER.BIN");
 	}
