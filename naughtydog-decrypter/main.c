83259691760624922a595f18ad325118bd3bcc22
hzhreal <142254293+hzhreal@users.noreply.github.com>
2025-12-18 00:00:54 +0100

    Update main.c
    Co-authored-by: Damián Parrino <bucanero@users.noreply.github.com>

--- naughtydog-decrypter/main.c
+++ naughtydog-decrypter/main.c
@@ -91,10 +91,7 @@ void init_savestate(SaveState *ss, u8 *data, size_t len, char type)
 
 void swap_u32_data(u8 *data, u32 size)
 {
-	size &= 0xFFFFFFFC;
-	u32 count = size / 4;
-	if (count == 0)
-		return;
+	u32 count = (size & 0xFFFFFFFC) / 4;
 	u32 *d = (u32 *)data;
 	for (u32 i = 0; i < count; i++)
 		d[i] = ES32(d[i]);
