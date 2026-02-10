e5abd4175a83aa2132202fe3f69c63bc4e271ca3
Damian Parrino <bucanero@users.noreply.github.com>
2026-02-10 13:44:30 -0300

    Update main.c

--- trails-cs-unpacker/main.c
+++ trails-cs-unpacker/main.c
@@ -309,6 +309,12 @@ static bool CompressType1(char* compressedData,
     return true;
 }
 
+static bool isVita(const char* data)
+{
+    // The uncompressed Vita save data start with 0x08000000 and have no CRC
+    return (ReadUInt32(data) == 8 && ReadUInt32(data + 12) == 0);
+}
+
 void* decompress_data(void* data, size_t* size)
 {
 	uint32_t original_size = ReadUInt32((char*)data);
@@ -333,7 +339,7 @@ void* decompress_data(void* data, size_t* size)
 	free(data);
 	*size = original_size;
 
-	int crc = calc_crc32(decompressed_data + 0x10, original_size - 0x10);
+	int crc = isVita(decompressed_data) ? 0 : calc_crc32(decompressed_data + 0x10, original_size - 0x10);
 	if (memcmp(&crc, decompressed_data + 0x0C, 4) != 0)
 		printf("[*] Warning: CRC32 Mismatch After Decompression!\n");
 	else
@@ -395,7 +401,7 @@ int main(int argc, char **argv)
 	u8* data;
 	char *opt, *bak;
 
-	printf("\ntrails-cs4-unpacker 0.1.0 - (c) 2026 by Bucanero\n\n");
+	printf("\nTrails of Cold Steel (Vita/PS4) Save Unpacker 0.2.0 - (c) 2026 by Bucanero\n\n");
 
 	if (--argc < 2)
 	{
@@ -424,11 +430,12 @@ int main(int argc, char **argv)
 	else
 	{
 		u32 crc = calc_crc32(data + 0x10, len - 0x10);
-
-		printf("[*] Stored Checksum: %08X\n", *(u32*)&data[12]);
-		printf("[*] Update Checksum: %08X\n", crc);
-		memcpy(data + 12, &crc, 4);
-
+		if (!isVita((char*)data))
+		{
+			printf("[*] Stored Checksum: %08X\n", *(u32*)&data[12]);
+			printf("[*] Update Checksum: %08X\n", crc);
+			memcpy(data + 12, &crc, 4);
+		}
 		data = compress_data(data, &len);
 	}
 
